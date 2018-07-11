10cf46a2362c849d5b58ee86034f7f13f26d397e
lilith4802 <lilith4802@gmail.com>
2018-07-11 17:17:19 +0900

    파일추가

--- 03_Data Science/2.Analysis/1.pandas_csv/2.csv_reader_parsing_and_write.py
+++ 03_Data Science/2.Analysis/1.pandas_csv/2.csv_reader_parsing_and_write.py
@@ -5,8 +5,7 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 with open(input_file, 'r', newline='') as csv_in_file:
     with open(output_file, 'w', newline='') as csv_out_file:
-        filereader = csv.reader(csv_in_file, delimither=',')
-        filewriter = csv.reader(csv_out_file, delimiter=',')
+        filereader = csv.reader(csv_in_file, delimiter=',')
+        filewriter = csv.writer(csv_out_file, delimiter=",")
         for row_list in filereader:
             filewriter.writerow(row_list)
-mapping = None
